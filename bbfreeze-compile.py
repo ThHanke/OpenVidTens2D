e0a88742c687cfdd23ded6776f79ebe088d2381c
Thomas Hanke <thomas.hanke82@gmail.com>
2014-05-26 15:05:46 +0200

    add icon files and setup.bat, bbfreeze-compile.py

---
+++ bbfreeze-compile.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from bbfreeze import Freezer
+from shutil import copy
+
+foldername='OpenVidTens2D64bit'
+
+f = Freezer(foldername, includes=('_strptime','_imaging'))
+f.addScript("OpenVidTens2D.py")
+f.use_compression=True
+f()    # starts the freezing process
+
+fileslist=('calicon.png',
+                          'logoOpenVidTens.png',
+                          'Startdownsmall.png',
+                          'Startupsmall.png',
+                          'Stopupsmall.png',
+                          'Stopdownsmall.png',
+                          'C:\Python27\Lib\helvetica-10.pil',
+                          'C:\Python27\Lib\helvB08.pil',
+                          'C:\Python27\Lib\helvetica-10.png',
+                          'C:\Python27\Lib\helvB08.png',
+           )
+
+for item in fileslist:
+    copy(item,foldername)
---
+++ setup.bat
@@ -0,0 +1,5 @@
+REM python setup.py py2exe
+python bbfreeze-compile.py
+REM Pyinstaller -y --onefile OpenVidTens2D.py
+REM Pyinstaller -y OpenVidTens2D.py
+pause
